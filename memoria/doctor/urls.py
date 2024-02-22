f46c7bffeb6d95538c8ea29307af497d3ef65fd2
mac <in5451@naver.com>
2024-02-23 06:33:43 +0900

    [modify] web develop file

--- memoria/doctor/urls.py
+++ memoria/doctor/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('doctor/submit_patient_info/', views.submit_patient_info, name='submit_patient_info'),
-    path('input/', views.input, name='input'),
-    path('output/<str:patient_id>/', views.output, name='output')
+    path('in/', views.input, name='input'),
+    path('out/', views.output, name='output')
 ]
